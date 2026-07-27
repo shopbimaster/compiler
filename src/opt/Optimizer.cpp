fd9a0d0fa0a1b1aac7da728adb5aae4012506d5c
YingM19 <165499697+YingM19@users.noreply.github.com>
2026-07-27 14:54:39 +0800

    perf(opt): 将循环除幂取余折叠为动态位提取

--- src/opt/Optimizer.cpp
+++ src/opt/Optimizer.cpp
@@ -56,6 +56,11 @@ void runO2(IR::Module* mod) {
     // 1a. 树摇
     treeShaking(mod);
 
+    if (repeatedDivRemToNative(mod)) {
+        constantFolding(mod);
+        deadCodeElimination(mod);
+    }
+
     if (recursiveModularMulToNative(mod)) {
         constantFolding(mod);
         deadCodeElimination(mod);
